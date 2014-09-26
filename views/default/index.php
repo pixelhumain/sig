b0a2fbd9422b0ad20c33f28a7d9ca38e2265a1d7
Kgneo <tristan.goguet@gmail.com>
2014-09-27 01:30:47 +1100

    refactor js & css + 2 map

--- views/default/index.php
+++ views/default/index.php
@@ -6,19 +6,22 @@ $cs->registerScriptFile(Yii::app()->request->baseUrl.'/js/api.js' , CClientScrip
 $cs->registerScriptFile(Yii::app()->request->baseUrl.'/js/vis.min.js' , CClientScript::POS_END);
 
 $cs->registerCssFile("http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.css");
-//$cs->registerCssFile(Yii::app()->theme->baseUrl. '/css/sig/leaflet.css');
-$cs->registerCssFile(Yii::app()->theme->baseUrl. '/css/sig/leaflet.draw.css');
-$cs->registerCssFile(Yii::app()->theme->baseUrl. '/css/sig/leaflet.draw.ie.css');
-$cs->registerCssFile(Yii::app()->theme->baseUrl. '/css/sig/MarkerCluster.css');
-$cs->registerCssFile(Yii::app()->theme->baseUrl. '/css/sig/MarkerCluster.Default.css');
-$cs->registerCssFile(Yii::app()->theme->baseUrl. '/css/sig/sig.css');
+$cs->registerCssFile($this->module->assetsUrl. '/css/leaflet.draw.css');
+$cs->registerCssFile($this->module->assetsUrl. '/css/leaflet.draw.ie.css');
+$cs->registerCssFile($this->module->assetsUrl. '/css/MarkerCluster.css');
+$cs->registerCssFile($this->module->assetsUrl. '/css/MarkerCluster.Default.css');
+$cs->registerCssFile($this->module->assetsUrl. '/css/sig.css');
+//$cs->registerCssFile($this->module->assetsUrl. '/css/leaflet.awesome-markers.css');
 
 $cs->registerScriptFile('http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.js' , CClientScript::POS_END);
-$cs->registerScriptFile(Yii::app()->theme->baseUrl.'/js/sig/leaflet.draw-src.js' , CClientScript::POS_END);
-$cs->registerScriptFile(Yii::app()->theme->baseUrl.'/js/sig/leaflet.draw.js' , CClientScript::POS_END);
-$cs->registerScriptFile(Yii::app()->theme->baseUrl.'/js/sig/leaflet.markercluster-src.js' , CClientScript::POS_END);
+$cs->registerScriptFile($this->module->assetsUrl.'/js/leaflet.draw-src.js' , CClientScript::POS_END);
+$cs->registerScriptFile($this->module->assetsUrl.'/js/leaflet.draw.js' , CClientScript::POS_END);
+$cs->registerScriptFile($this->module->assetsUrl.'/js/leaflet.markercluster-src.js' , CClientScript::POS_END);
+//$cs->registerScriptFile($this->module->assetsUrl.'/js/leaflet.awesome-markers.min.js' , CClientScript::POS_END);
 
 $this->pageTitle=$this::moduleTitle;
+
+
 ?>
 <style type="text/css">
  .mapCanvasSlider{
@@ -50,9 +53,7 @@ $this->pageTitle=$this::moduleTitle;
  .popup-img-profil{
  
  }
- 
- 
- 
+
  .popup-info-profil{
  	width:100%;
  	text-align:center;
@@ -60,6 +61,7 @@ $this->pageTitle=$this::moduleTitle;
  }
  .popup-info-profil-username{
  	width:100%;
+ 	text-transform:capitalize;
  	text-align:center;
  	font-size:30px;
  	color:yellow;
@@ -98,15 +100,13 @@ $this->pageTitle=$this::moduleTitle;
     	<li style="background-color: #3b4a52; height:70%;">
         	<div class="slide-content" style="width:100%; left:0px; height:100%;">
             	<div class="mapCanvasSlider" id="mapCanvasSlide1">
-            	
-        		</div>
+            	</div>
         	</div>
     	</li>
     	<li style="background-color: #3b4a52; height:70%;">
         	<div class="slide-content" style="width:100%; left:0px; height:100%;">
-            	<div class="mapCanvasSlider" id="mapCanvasSlide2">
-            	
-        		</div>
+        		<div class="mapCanvasSlider" id="mapCanvasSlide2">
+            	</div>
         	</div>
     	</li>
     </ul>
@@ -127,17 +127,18 @@ $(document).ready( function()
 		var map = L.map(canvasId).setView([51.505, -0.09], 4);
 
 		L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png', {
-	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-	subdomains: 'abcd',
-	minZoom: 0,
-	maxZoom: 20
-	}).setOpacity(0.4).addTo(map);
-	
-	//u.osmfr.org/m/5061/
+			attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+			subdomains: 'abcd',
+			minZoom: 0,
+			maxZoom: 20
+		}).setOpacity(0.4).addTo(map);
 	
 		map.on('click', function(e) {
-    		alert(map.getCenter()); // e is an event object (MouseEvent in this case)
+    		//alert(map.getCenter());
 		});
+		
+		
+		
 		return map;
 	}								
 	//##
@@ -153,24 +154,35 @@ $(document).ready( function()
 		var marker = L.marker([options.lat, options.lng], markerOptions).addTo(thisMap)
 		.bindPopup(contentString);
 		//.openPopup();
-
+			
 		return marker;
 	}							
 	//##
 	//récupère le nom de l'icon en fonction du type de marker souhaité
 	function getIcoMarker(type){
-		if(type == "citoyen") 	return L.icon({ iconUrl: "/ph/images/sig/markers/02_ICON_CITOYENS.png",
+		/*
+		if(type == "citoyen")
+		return L.AwesomeMarkers.icon({ prefixe:'fa', icon: 'fa-circle', iconColor:"white" });
+  		
+		if(type == "pixelactif")
+		return L.AwesomeMarkers.icon({ prefixe:'fa', icon: 'fa-circle', iconColor:"white" });
+  		
+		if(type == "partenaire")
+		return L.AwesomeMarkers.icon({ prefixe:'fa', icon: 'lightbulb-o', iconColor:"white" });
+  		*/ 
+  		
+		if(type == "citoyen") 	return L.icon({ iconUrl: "<?php echo $this->module->assetsUrl.'/image/markers/02_ICON_CITOYENS.png'; ?>",
 												iconSize: 		[14, 14],
 												iconAnchor: 	[7, 7],
 												popupAnchor: 	[0, -10] });
 												
 		
-		if(type == "pixelactif") 		return L.icon({ iconUrl: "/ph/images/sig/markers/02_ICON_PIXEL_ACTIF.png",
+		if(type == "pixelActif") 		return L.icon({ iconUrl: "/ph/images/sig/markers/02_ICON_PIXEL_ACTIF.png",
 												iconSize: 		[14, 14],
 												iconAnchor: 	[7, 7],
 												popupAnchor: 	[0, -10] });	
 												
-		if(type == "partenaire") 	return L.icon({ iconUrl: "/ph/images/sig/markers/02_ICON_PARTENAIRES.png",
+		if(type == "partnerPH") 	return L.icon({ iconUrl: "/ph/images/sig/markers/02_ICON_PARTENAIRES.png",
 												iconSize: 		[14, 16],
 												iconAnchor: 	[7, 7],
 												popupAnchor: 	[0, -10] });						  						
@@ -193,26 +205,28 @@ $(document).ready( function()
 	}
 	
 	
+	
 
-				function initClusterMap(canvas){
-					if(mapClusters != null) mapClusters.remove();
-					
-					mapClusters = loadMap(canvas);
-					markersLayer = new L.MarkerClusterGroup();
-					mapClusters.addLayer(markersLayer);// add it to the map
-					geoJsonCollection = { type: 'FeatureCollection', features: new Array() };
-				}
-				
 				
 				//##
 				//affiche les citoyens qui possèdent des attributs geo.latitude, geo.longitude, depuis la BD
-				function showCitoyensClusters(mapClusters){ 
+				function showCitoyensClusters(mapClusters, origine){ 
 					
-					geoJsonCollection = { type: 'FeatureCollection', features: new Array() };
-					markersLayer.clearLayers();			
+					var markersLayer = new L.MarkerClusterGroup();
+					mapClusters.addLayer(markersLayer);
+	
+					var geoJsonCollection = { type: 'FeatureCollection', features: new Array() };
+					//markersLayer.clearLayers();			
 					
-					testitget("showCitoyensResult", '/ph/<?php echo $this::$moduleKey?>/api/showCitoyens',
-						function (data){
+					var bounds = mapClusters.getBounds();
+					var params = {};
+					params["latMinScope"] = bounds.getSouthWest().lat;
+					params["lngMinScope"] = bounds.getSouthWest().lng;
+					params["latMaxScope"] = bounds.getNorthEast().lat;
+					params["lngMaxScope"] = bounds.getNorthEast().lng;
+				
+					testitpost("showCitoyensResult", '/ph/<?php echo $this::$moduleKey?>/api/' + origine, params,
+						function (data){ //alert(JSON.stringify(data));
 							//var listItemMap = "";
 						 	$.each(data, function() {  	
 								if(this['geo'] != null){
@@ -221,7 +235,7 @@ $(document).ready( function()
 									
 									//THUMB PHOTO PROFIL
 				 					var content = "";
-				 					if(this['thumb'] != null)   
+				 					if(this['thumb_path'] != null)   
 				 					content += 	"<img src='" + this['thumb_path'] + "' height=70 class='popup-info-profil-thumb'>";
 				 					
 				 					//NOM DE L'UTILISATEUR
@@ -230,7 +244,9 @@ $(document).ready( function()
 				 					
 				 					//TYPE D'UTILISATEUR (CITOYEN, ASSO, PARTENAIRE, ETC)
 				 					var typeName = this['type'];
-				 					if(this['type'] == null)  type = "Citoyen";
+				 					if(this['type'] == null)  typeName = "Citoyen";
+				 					if(this['name'] == null)  typeName += " anonyme";
+				 					
 				 					content += 	"<div class='popup-info-profil-usertype'>" + typeName + "</div>";
 				 					
 				 					//WORK - PROFESSION
@@ -240,6 +256,10 @@ $(document).ready( function()
 				 					//URL
 				 					if(this['url'] != null)     
 				 					content += 	"<div class='popup-info-profil-url'>" + this['url'] + "</div>";
+				 					
+				 					//CODE POSTAL
+				 					if(this['cp'] != null)     
+				 					content += 	"<div class='popup-info-profil'>" + this['cp'] + "</div>";
 				 					
 				 					//VILLE ET PAYS
 				 					var place = this['city'];
@@ -257,6 +277,7 @@ $(document).ready( function()
 				 					//création de l'icon sur la carte
 				 					var type;
 				 					if(this['type'] != null) type = this['type'];
+				 					else if(this['tags'] != null) type = this['tags'];
 				 					else type = "citoyen";
 				 					
 				 					var properties = { 	title : this['name'], 
@@ -274,14 +295,20 @@ $(document).ready( function()
 							//affiche la liste d'éléments à droite de la carte
 							//$('#mapListItems').html(listItemMap);
 							
-							var points_rand = L.geoJson(geoJsonCollection, {
+							var points = L.geoJson(geoJsonCollection, {
 							onEachFeature: function (feature, layer) {				   //Sur chaque marker
 									layer.bindPopup(feature["properties"]["content"]); //ajoute la bulle d'info avec les données
 									layer.setIcon(feature["properties"]["icon"]);	   //et affiche l'icon demandé
+									layer.on('mouseover', function(e) {
+										layer.openPopup();
+									});
+									layer.on('mouseout', function(e) {
+										layer.closePopup();
+									});
 								}
 							});
 						
-							markersLayer.addLayer(points_rand); 	// add it to the cluster group
+							markersLayer.addLayer(points); 	// add it to the cluster group
 							mapClusters.addLayer(markersLayer);		// add it to the map
 							//mapClusters.fitBounds(markersLayer.getBounds()); //set view on the cluster extend					
 						});
@@ -300,20 +327,15 @@ $(document).ready( function()
 	
 	var map1 = loadMap("mapCanvasSlide1");
 	map1.setView([30.29702, -21.97266], 3);
-	addMarkerList(map1);
+	showCitoyensClusters(map1, "getPixelActif");
+	//addMarkerList(map1);
 	
+		
+		
 	
 	var map2 = loadMap("mapCanvasSlide2");
 	map2.setView([-21.13318, 55.5314], 10);
-	
-	var markersLayer;	
-	var geoJsonCollection;
-							
-	markersLayer = new L.MarkerClusterGroup();
-	map2.addLayer(markersLayer);
-	geoJsonCollection = { type: 'FeatureCollection', features: new Array() };
-					
-	showCitoyensClusters(map2);
+	showCitoyensClusters(map2, "getCommunected");
 
 
 });
